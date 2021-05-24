01dd3f8577287cbabfb37b215a3bcbb467231acd
wbluke <pwb0127@gmail.com>
2021-05-25 00:06:02 +0900

    feat: append aws cloudfront step at deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,8 @@ echo "############# npm run build #############"
 npm run build
 
 echo "############# deploy to s3 #############"
-aws s3 cp --recursive ./build s3://woowa-sweetpotato.xyz/
+aws s3 sync ./build s3://woowa-sweetpotato.xyz/
+
+echo "############# invalidate cloudfront #############"
+aws cloudfront create-invalidation --distribution-id E39AB0OB2RJ49 --paths "/*"
 
